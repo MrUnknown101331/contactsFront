8a13b4300f875739a6dad48baf7a55c24749aa86
MrUnknown101331 <faizanahmed0107@gmail.com>
2024-09-25 17:55:28 +0530

    smoothened the animation

--- src/Login_Register/Login.jsx
+++ src/Login_Register/Login.jsx
@@ -1,10 +1,12 @@
 import styles from './Login.module.css'
 import PropTypes from 'prop-types'
+import {useEffect, useState} from "react";
 
 function Login(props) {
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const login = (
-        <div className={styles.login_container}>
+        <>
             <h1 className={styles.heading}>Log In</h1>
             <p className={styles.text}>Enter your username and password to login</p>
             <form className={styles.form_container}>
@@ -12,11 +14,12 @@ function Login(props) {
                 <input type="password" className={styles.userInput} name="password" placeholder="Pasword" required/>
                 <button type="submit" className={styles.button}>log in</button>
             </form>
-        </div>
+        </>
     );
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const signup = (
-        <div className={`${styles.login_container} ${styles.toggle_right}`}>
+        <>
             <h1 className={styles.heading}>Create Account</h1>
             <p className={styles.text}>use your email for registeration</p>
             <form className={styles.form_container}>
@@ -25,10 +28,21 @@ function Login(props) {
                 <input type="password" className={styles.userInput} name="password" placeholder="Pasword" required/>
                 <button type="submit" className={styles.button}>sign up</button>
             </form>
-        </div>
+        </>
     );
 
-    return (props.signUp ? signup : login);
+    const [current, setCurrent] = useState(login);
+    useEffect(() => {
+        setTimeout(() => {
+            setCurrent(props.signUp ? signup : login);
+        }, 250)
+    }, [login, props.signUp, signup]);
+
+    return (
+        <div className={`${styles.login_container} ${props.signUp ? styles.toggle_right : ' '}`}>
+            {current}
+        </div>
+    );
 }
 
 Login.propTypes = {
--- src/Login_Register/Side.jsx
+++ src/Login_Register/Side.jsx
@@ -1,25 +1,40 @@
 import styles from './Side.module.css'
 import PropTypes from "prop-types";
+import {useEffect, useState} from "react";
 
 function Side(props) {
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const login = (
-        <div className={styles.side}>
+        <div className={styles.container}>
             <h1 className={styles.heading}>Hello, Friend!</h1>
             <p className={styles.text}>Register with your personal details to use all of site features</p>
             <button className={styles.button} onClick={props.toggleSignUp}>Sign up</button>
         </div>
     );
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const signup = (
-        <div className={`${styles.side} ${styles.toggle_left}`}>
+        <div className={styles.container}>
             <h1 className={styles.heading}>Welcome Back!</h1>
             <p className={styles.text}>Enter your personal details to use all of site features</p>
             <button className={styles.button} onClick={props.toggleSignUp}>Log in</button>
         </div>
     );
 
-    return (props.signUp ? signup : login);
+    const [current, setCurrent] = useState(login);
+    useEffect(() => {
+        setTimeout(() => {
+            setCurrent(props.signUp ? signup : login);
+        }, 200)
+
+    }, [login, props.signUp, signup]);
+
+    return (
+        <div className={`${styles.side} ${props.signUp ? styles.toggle_left : ' '}`}>
+            {current}
+        </div>
+    );
 }
 
 Side.propTypes = {
